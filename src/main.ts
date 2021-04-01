386e601e319cb6f966f189f2ea684cb1fd42f3a4
Jonas Strassel <info@jonas-strassel.de>
2021-04-01 14:40:57 +0000

    fix: prepare action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const inactiveDays = parseInt(core.getInput('inactiveDays'))
+    // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.debug(
+      `searching for check suites that have not been run for ${inactiveDays} days`
+    )
   } catch (error) {
     core.setFailed(error.message)
   }
