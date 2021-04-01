f5c790da966018fe05c7cded0b54d30376bdb9e3
Jonas Strassel <info@jonas-strassel.de>
2021-04-01 15:01:22 +0000

    fix: adapt some texts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import * as core from '@actions/core'
 async function run(): Promise<void> {
   try {
     const inactiveDays = parseInt(core.getInput('inactiveDays'))
-    // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    core.debug(`searching for check suites that have not been run for ${inactiveDays} days`) 
+    // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.debug(
+      `searching for check suites that have not been run for ${inactiveDays} days`
+    )
   } catch (error) {
     core.setFailed(error.message)
   }
